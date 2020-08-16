914dcb2f0ba9d6816ba81273fbc7464fafe0ef17
keyeypee <keyeypee@gmail.com>
2020-08-16 18:37:12 +0530

    Updates

---
+++ precache-manifest.2e84dc68f778559a21b1c806ed6bbae5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1457a3db5d031c369ff37b4e2cac08bf",
+    "url": "/album-list/index.html"
+  },
+  {
+    "revision": "75e45f990e0aa04b7cf7",
+    "url": "/album-list/static/css/2.3ea6c7db.chunk.css"
+  },
+  {
+    "revision": "281306d784d0d6a5b641",
+    "url": "/album-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "75e45f990e0aa04b7cf7",
+    "url": "/album-list/static/js/2.4d3dc18d.chunk.js"
+  },
+  {
+    "revision": "ccb736d04c3368cb01e18da094e70f4d",
+    "url": "/album-list/static/js/2.4d3dc18d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281306d784d0d6a5b641",
+    "url": "/album-list/static/js/main.4e9a9c0b.chunk.js"
+  },
+  {
+    "revision": "914e5f16837d3a3e05d8",
+    "url": "/album-list/static/js/runtime-main.e9931edb.js"
+  }
+]);
